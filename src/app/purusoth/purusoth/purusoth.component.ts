b88a8e408838bd579f17fb97fcf8df5645dac1e8
sugumarsakthi <sugumarsakthi95@gmail.com>
2024-07-01 21:44:09 +0530

    interest amount updated

--- src/app/purusoth/purusoth/purusoth.component.ts
+++ src/app/purusoth/purusoth/purusoth.component.ts
@@ -16,7 +16,31 @@ export class PurusothComponent {
   displayedColumns: string[] = ['#','date','amount','status'];
   PurusothInt = Purusothinterests;
 
+  public interestamount:any;  
+  interestamounts:any;
+  pendingamount:any;
+  totalintamount:any; 
   ngOnInit(){
+    this.interestamount = this.PurusothInt;
+    this.interestamounts = 0; 
+    this.pendingamount = 0;   
+    this.totalintamount = 0;   
+    for(var i=0; i < this.interestamount.length; i++){   
+      if(this.interestamount[i].status == "Done"){
+        this.interestamounts += this.interestamount[i].amount;
+        console.log(this.interestamounts, "interesttotalamount");
+      }
+    }
+    for(var i=0; i < this.interestamount.length; i++){   
+      if(this.interestamount[i].status == "In Progress"){
+        this.pendingamount += this.interestamount[i].amount;
+        console.log(this.pendingamount, "interesttotalamount");
+      }
+    }
+    for(var i=0; i < this.interestamount.length; i++){   
+        this.totalintamount += this.interestamount[i].amount;
+        console.log(this.totalintamount, "interesttotalamount");
+    }
     this.createPurusothbarchart();
   }
   createPurusothbarchart(){
